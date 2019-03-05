d160343fed0a03245aa1f6fa68f0538f2d25998c
jmuldvp <jmul.dvp@gmail.com>
2019-03-04 21:02:19 -0700

    CP7 completed

--- lib/bloc_record/base.rb
+++ lib/bloc_record/base.rb
@@ -6,6 +6,7 @@ require 'bloc_record/persistence'
 require 'bloc_record/selection'
 require 'bloc_record/connection'
 require 'bloc_record/collection'
+require 'bloc_record/associations'
 
 module BlocRecord
   class Base
@@ -13,6 +14,7 @@ module BlocRecord
     extend Selection
     extend Schema
     extend Connection
+    extend Associations
 
     def initialize(options={})
       options = BlocRecord::Utility.convert_keys(options)
