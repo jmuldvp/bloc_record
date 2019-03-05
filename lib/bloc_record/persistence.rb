c29fd7b2eb19fd8bcd04c3abcdcf99228d980de2
jmuldvp <jmul.dvp@gmail.com>
2019-03-04 19:37:47 -0700

    CP6 completed

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -37,6 +37,10 @@ module Persistence
     self.class.update(self.id, updates)
   end
 
+  def destroy
+    self.class.destroy(self.id)
+  end
+
   module ClassMethods
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
@@ -87,6 +91,34 @@ module Persistence
     def method_missing(method, *args, &block)
       unless method != :update_name
         update(self.id, {name: args[0]})
+      end
+    end
+
+    def destroy(*id)
+      if id.length > 1
+        where_clause = "WHERE id IN (#{id.join(",")});"
+      else
+        where_clause = "WHERE id = #{id.first};"
+      end
+
+      connection.execute <<-SQL
+        DELETE FROM #{table} #{where_clause}
+      SQL
+    end
+
+    def destroy_all
+      if conditions_hash && !conditions_hash.empty?
+        conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+        conditions = conditions_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+          WHERE #{conditions};
+        SQL
+      else
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+        SQL
       end
     end
 
