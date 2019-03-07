f8a32c9e5308d9910637ca5f055f34b2d9a67a47
jmuldvp <jmul.dvp@gmail.com>
2019-03-06 19:25:01 -0700

    CP7 assignment completed

--- lib/bloc_record/connection.rb
+++ lib/bloc_record/connection.rb
@@ -2,6 +2,10 @@ require 'sqlite3'
 
 module Connection
   def connection
-    @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    if BlocRecord.database_type == :sqlite3
+      @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    elsif BlocRecord.database_type == :pg
+      @connection = PG.connect(dbname: BlocRecord.database_filename)
+    end
   end
 end
